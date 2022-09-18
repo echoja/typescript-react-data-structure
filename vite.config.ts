ae59c8c6fc0baed473e7752b09b42e9bd854e6e6
Kim Tae Hoon <eszqsc112@gmail.com>
2022-09-18 10:51:11 +0900

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import codesandbox from "@gsimone/codesandbox-vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/typescript-react-data-structure/",
   plugins: [react(), codesandbox()],
 });
